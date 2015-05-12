6843462320316a9a685945898ca6f3d873ff2556
satya <satya.nani.40@gmail.com>
2015-05-13 02:13:50 +0530

    added views

---
+++ wsgi/myproject/myproject/models.py
@@ -0,0 +1,19 @@
+from mongoengine import *
+connect('test')
+from mongoengine.django.auth import User
+from rest_framework import serializers
+
+from rest_framework_mongoengine.serializers import DocumentSerializer
+
+class Chapterdetails(Document):
+    chapter_name = StringField(max_length=200)
+    chapter_path = StringField(max_length=500)
+
+class Examdetails(Document):
+    exam_name = StringField(max_length= 25000)
+    question = StringField(max_length= 25000)
+    a = StringField(max_length= 2500)
+    b = StringField(max_length= 2500)
+    c = StringField(max_length= 2500)
+    d = StringField(max_length= 2500)
+    correct = StringField(max_length= 250)
